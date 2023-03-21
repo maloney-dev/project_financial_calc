7e990b1d662270fb2557c5cdf35a200f5be70323
Nicolas Maloney <nicolasmaloneyf@gmail.com>
2023-03-21 15:16:30 -0300

    added the output

--- calculadora_financeira_simples.py
+++ calculadora_financeira_simples.py
@@ -2,6 +2,8 @@
 print("Quer descobrir quanto terá de pagar?")
 presentValue = int(input("Qual é o valor atual? R$"))
 taxaJuros = int(input("Qual é a taxa de juros? "))
-tempo = int(input("Em quantos meses vai pagar? "))
+tempo = int(input("Em quantos meses ou anos vai pagar? "))
 juros = taxaJuros / 100 
 vF = presentValue * ((1 + juros)**tempo)
+
+print(f"O valor final a ser pago será de: R${vF:.2f}")
