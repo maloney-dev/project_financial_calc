03ced3412c01cd5fb715f99f48bc65ea819311a9
Nicolas Maloney <nicolasmaloneyf@gmail.com>
2023-03-21 22:44:40 -0300

    variables to calc the present value

--- calculadora_financeira_simples.py
+++ calculadora_financeira_simples.py
@@ -1,10 +1,20 @@
 #Receives user input on present and future values, months and financial tax 
 print("Quer descobrir quanto terá de pagar?")
 presentValue = int(input("Qual é o valor atual? R$"))
-taxaJuros = int(input("Qual é a taxa de juros? "))
-tempo = int(input("Em quantos meses ou anos vai pagar? "))
+taxaJuros = float(input("Qual é a taxa de juros? "))
+tempo = int(input("Em quanto tempo (meses ou anos) vai pagar? "))
 juros = taxaJuros / 100 
 vF = presentValue * ((1 + juros)**tempo)
 
 #Prints future value output 
 print(f"O valor final a ser pago será de: R${vF:.2f}")
+
+#Discover the present value with the final value and time variation
+
+finalValue = int(input("Qual é o valor atual? R$"))
+taxaJuros = int(input("Qual é a taxa de juros? "))
+tempo = int(input("Em quanto tempo (meses ou anos) vai pagar? "))
+juros = taxaJuros / 100
+pV = finalValue / ((1 + juros)**tempo)
+
+print(f"O valor inicial a ser pago será de: R${pV:.2f}")
